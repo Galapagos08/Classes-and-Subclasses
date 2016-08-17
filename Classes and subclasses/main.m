764feafe814f7ed79459308e1520ac6baf3bb19a
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-17 14:57:10 -0400

    Initial Commit

---
+++ Classes and subclasses/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Classes and subclasses
+//
+//  Created by Dan Esrey on 2016/17/08.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
